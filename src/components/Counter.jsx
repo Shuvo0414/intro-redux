10aa4b1bd9b0fd59eb02d4e1683fdbe684a06258
Shuvo0414 <138393384+Shuvo0414@users.noreply.github.com>
2024-02-15 12:14:10 +0600

    Enhance Counter component and Redux slice
    - Added 'incrementByValue' action to counterSlice.js, allowing incremental value adjustments
    - Updated Counter component to include a button that increments the count by 5 using 'incrementByValue'

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,15 +1,38 @@
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  decrement,
+  increment,
+  incrementByValue,
+} from "../redux/features/counter/counterSlice";
 
 const Counter = () => {
   const { count } = useSelector((state) => state.counter);
+  const dispatch = useDispatch();
 
   return (
     <>
-      <button>Increment</button>
+      <button
+        className="mb-4 px-4 py-2 bg-green-400 text-white font-bold text-lg rounded-sm"
+        onClick={() => dispatch(incrementByValue(5))}
+      >
+        Increment By 5
+      </button>
+      <br />
+      <button
+        className="mb-4 px-4 py-2 bg-green-400 text-white font-bold text-lg rounded-sm"
+        onClick={() => dispatch(increment())}
+      >
+        Increment
+      </button>
       <div>
-        <h1>{count}</h1>
+        <h1 className=" text-xl font-extrabold">{count}</h1>
       </div>
-      <button>Decrement</button>
+      <button
+        className="mt-4 px-4 py-2 bg-red-400 text-white font-bold text-lg rounded-sm"
+        onClick={() => dispatch(decrement())}
+      >
+        Decrement
+      </button>
     </>
   );
 };
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,8 +2,7 @@ import Counter from "../components/Counter";
 
 const Home = () => {
   return (
-    <div>
-      <h1>This is home</h1>
+    <div className=" container mx-auto text-center mt-52">
       <Counter></Counter>
     </div>
   );
